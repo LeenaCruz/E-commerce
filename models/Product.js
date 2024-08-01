f059855ad11455367c59865e419bdbe80cace077
Leena <leenacruz@gmail.com>
2024-07-31 23:17:30 -0700

    Add routes

--- models/Product.js
+++ models/Product.js
@@ -34,7 +34,14 @@ price: {
     validate: {
       isNumeric: true
     }
-  }
+  }, 
+  category_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'category',
+      key: 'id',
+    },
+  },
 },
   {
     sequelize,
